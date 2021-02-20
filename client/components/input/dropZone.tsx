48b4988af76a4f65ece76b883b087b6ffd451193
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-02-20 11:39:29 +0530

    add file input button

--- client/components/input/dropZone.tsx
+++ client/components/input/dropZone.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState, DragEvent } from 'react';
+import { useCallback, useState, useRef } from 'react';
 import {
 	Box,
 	BoxProps,
@@ -14,22 +14,21 @@ import {
 import { VscCheck, VscClose } from 'react-icons/vsc';
 import { MdError } from 'react-icons/md';
 
-import { getFileDatas,FileInfoTypes} from './filesHandler';
+import { getFileDatas, FileInfoTypes } from './filesHandler';
 
 export default function DropZone() {
 	const [fileInfos, setFileInfos] = useState<FileInfoTypes[]>([]);
 
-	const handleOnDrop = useCallback(async (e: DragEvent<HTMLDivElement>) => {
-		e.preventDefault();
-
-		const { fileInfos } = await getFileDatas(e.dataTransfer.files);
+	const handleAdd = useCallback(async (files: FileList | null) => {
+		if (!files || files.length === 0) return;
+		const { fileInfos } = await getFileDatas(files);
 		setFileInfos((prev) => prev.concat(fileInfos));
 	}, []);
 
 	const handleRemove = useCallback(
 		(idx: number) => {
 			const tempArray = [...fileInfos];
-			tempArray.splice(idx,1);
+			tempArray.splice(idx, 1);
 			setFileInfos(tempArray);
 		},
 		[fileInfos]
@@ -39,11 +38,15 @@ export default function DropZone() {
 
 	return (
 		<Box my="8">
-			<DropZoneCatcher onDrop={handleOnDrop}>
+			<DropZoneCatcher
+				onDrop={(e) => {
+					e.preventDefault();
+					handleAdd(e.dataTransfer.files);
+				}}>
 				{fileInfos.length > 0 ? (
 					<DropZoneTable statuses={fileInfos} handleRemove={handleRemove} />
 				) : (
-					<DropZoneInitialText />
+					<DropZoneInitialText handleAdd={handleAdd} />
 				)}
 			</DropZoneCatcher>
 		</Box>
@@ -71,14 +74,27 @@ function DropZoneCatcher(props: BoxProps) {
 	);
 }
 
-function DropZoneInitialText() {
+function DropZoneInitialText({ handleAdd }: IDropZoneInitialText) {
+	const inputRef = useRef<HTMLInputElement>(null);
+
 	return (
 		<>
 			<Text fontSize="4xl" fontWeight="bold" color="facebook">
 				Drag and Drop Files Here
 			</Text>
-			<Button d="block" colorScheme="telegram" mt="2.5">
-				Or Select the Files
+			<Button
+				d="block"
+				colorScheme="telegram"
+				mt="2.5"
+				onClick={() => inputRef.current?.click()}>
+				Or Click to Upload Files
+				<input
+					type="file"
+					multiple
+					ref={inputRef}
+					style={{ display: 'none' }}
+					onChange={(e) => handleAdd(e.target.files)}
+				/>
 			</Button>
 		</>
 	);
@@ -175,4 +191,8 @@ function DropZoneTableHeader() {
 interface IDropZoneTableProps {
 	statuses: FileInfoTypes[];
 	handleRemove: (id: number) => void;
+}
+
+interface IDropZoneInitialText {
+	handleAdd: (files: FileList | null) => Promise<void>;
 }
